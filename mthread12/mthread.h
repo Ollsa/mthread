8e407f9e6089b1e55e619835e74bc96000493747
Ollsa <72123305+Ollsa@users.noreply.github.com>
2021-06-02 04:20:51 +0700

    Update mthread.h

--- mthread12/mthread.h
+++ mthread12/mthread.h
@@ -1,1 +1,8 @@
 #pragma once
+
+struct Parameter
+{
+	unsigned int fifoSize;
+	unsigned int dataBlockSize;
+	Parameter(unsigned int _fifoSize, unsigned int _dataBlockSize) : fifoSize(_fifoSize), dataBlockSize(_dataBlockSize) {};
+};
